b522506006cd448481ee21ffbf26fb757eb3dd81
Andres Mandado <andres.mandado-almajano@here.com>
2020-09-29 20:17:25 +0200

    HARP-11672: Refactor WebTileDataSource to support request cancelation.
    New WebTileLoader class implements ITileLoader, allowing web tile loading
    to be cancelled.
    
    Extract common tile loader functionality to TileLoaderBase abstract class.

--- @here/harp-mapview-decoder/test/TileLoaderTest.ts
+++ @here/harp-mapview-decoder/test/TileLoaderTest.ts
@@ -15,15 +15,15 @@ import {
     webMercatorTilingScheme
 } from "@here/harp-geoutils";
 import { DataSource, MapView, Statistics, Tile, TileLoaderState } from "@here/harp-mapview";
+import { DataProvider, TileLoader } from "@here/harp-mapview-decoder";
 import { LoggerManager } from "@here/harp-utils";
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as sinon from "sinon";
-chai.use(chaiAsPromised);
-const { expect } = chai;
-import { DataProvider, TileLoader } from "../index";
 
+chai.use(chaiAsPromised);
 // Needed for using expect(...).true for example
+const { expect } = chai;
 
 class MockDataSource extends DataSource {
     /** @override */
@@ -119,8 +119,7 @@ describe("TileLoader", function() {
                 dataSource,
                 tileKey,
                 dataProvider,
-                new MockTileDecoder(),
-                0
+                new MockTileDecoder()
             );
 
             const loadPromise = tileLoader.loadAndDecode();
@@ -134,8 +133,7 @@ describe("TileLoader", function() {
                 dataSource,
                 tileKey,
                 dataProvider,
-                new MockTileDecoder(),
-                0
+                new MockTileDecoder()
             );
 
             const loadPromise = tileLoader.loadAndDecode();
@@ -151,7 +149,7 @@ describe("TileLoader", function() {
         it("should handle empty payloads", function() {
             const tileDecoder = new MockTileDecoder();
             const decodeTileSpy = sinon.spy(tileDecoder, "decodeTile");
-            const tileLoader = new TileLoader(dataSource, tileKey, dataProvider, tileDecoder, 0);
+            const tileLoader = new TileLoader(dataSource, tileKey, dataProvider, tileDecoder);
 
             const getTileStub = sinon.stub(dataProvider, "getTile").resolves(new ArrayBuffer(0));
             let loadPromise = tileLoader.loadAndDecode();
@@ -179,8 +177,7 @@ describe("TileLoader", function() {
                     dataSource,
                     tileKey,
                     dataProvider,
-                    new MockTileDecoder(),
-                    0
+                    new MockTileDecoder()
                 );
                 LoggerManager.instance.update("TileLoader", { enabled: false });
             });
@@ -218,8 +215,7 @@ describe("TileLoader", function() {
                 dataSource,
                 tileKey,
                 dataProvider,
-                new MockTileDecoder(),
-                0
+                new MockTileDecoder()
             );
 
             const loadPromise = tileLoader.loadAndDecode();
@@ -236,14 +232,13 @@ describe("TileLoader", function() {
                 dataSource,
                 tileKey,
                 dataProvider,
-                new MockTileDecoder(),
-                0
+                new MockTileDecoder()
             );
             const loadPromise = tileLoader.loadAndDecode();
             expect(loadPromise).to.not.be.undefined;
 
             // mock loaded data state
-            tileLoader.payload = new ArrayBuffer(5);
+            (tileLoader as any).payload = new ArrayBuffer(5);
             tileLoader.state = TileLoaderState.Loaded;
             (tileLoader as any).startDecodeTile();
 
